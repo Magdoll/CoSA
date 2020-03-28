a9979e747841873745ddff9b83674782375ed738
Magdoll <magdoll@gmail.com>
2020-03-27 19:13:46 -0700

    v1.0.0

--- R/summarize_metadata.R
+++ R/summarize_metadata.R
@@ -11,11 +11,11 @@ args <- commandArgs(trailingOnly = TRUE)
 csv.file <- args[1]
 
 report.file <- "gisaid_metadata_report.pdf"
-report.figdir <- "gisaid_figures"
-
-if (!file.exists(report.figdir)){
-    dir.create(report.figdir)
-}
+#report.figdir <- "gisaid_figures"
+#
+#if (!file.exists(report.figdir)){
+#    dir.create(report.figdir)
+#}
 
 #********************** Packages (install if not found)
 
@@ -34,26 +34,27 @@ library(dplyr)
 # plot by specimen source
 # -----------------------------------------
 x.all <- read.table(csv.file,sep=',',header=T)
+x <- x.all[x.all$Host=='Human',]
 p.source <- ggplot(x, aes(x=Specimen.source, fill=Specimen.source)) + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1),legend.position="none") + labs(title="Specimen Source") + xlab("") + ylab("Count")
-ggsave(file.path(report.figdir,'Rplot.specimen_source.png'), dpi=200, width=4, height=2.5)
+#ggsave(file.path(report.figdir,'Rplot.specimen_source.png'), dpi=200, width=6, height=4)
 # -----------------------------------------
 # plot geography
 # -----------------------------------------
 p.continent <- ggplot(x, aes(Continent, fill=Continent)) + geom_bar() + coord_flip() +
   xlab("") + ylab("") + labs(title="Sequences by Continent") + theme(legend.position="none")
-ggsave(file.path(report.figdir,'Rplot.geography_byContinent.png'), dpi=200, width=4, height=2.5)
+#ggsave(file.path(report.figdir,'Rplot.geography_byContinent.png'), dpi=200, width=6, height=4)
 
 p.asia <- ggplot(x[x$Continent=='Asia',], aes(Country, fill=Country)) + geom_bar() + coord_flip() +
   xlab("") + ylab("") + labs(title="Sequences by Country, Asia") + theme(legend.position="none")
-ggsave(file.path(report.figdir,'Rplot.geography_asia.png'), dpi=200, width=4, height=2.5)
+#ggsave(file.path(report.figdir,'Rplot.geography_asia.png'), dpi=200, width=6, height=4)
 
 p.europe <- ggplot(x[x$Continent=='Europe',], aes(Country, fill=Country)) + geom_bar() + coord_flip() +
   xlab("") + ylab("") + labs(title="Sequences by Country, Europe") + theme(legend.position="none")
-ggsave(file.path(report.figdir,'Rplot.geography_europe.png'), dpi=200, width=4, height=2.5)
+#ggsave(file.path(report.figdir,'Rplot.geography_europe.png'), dpi=200, width=6, height=4)
 
 p.na <- ggplot(x[x$Continent=='North America',], aes(Location, fill=Location)) + geom_bar() + coord_flip() +
   xlab("") + ylab("") + labs(title="Sequences in NA") + theme(legend.position="none")
-ggsave(file.path(report.figdir,'Rplot.geography_na.png'), dpi=200, width=4, height=2.5)
+#ggsave(file.path(report.figdir,'Rplot.geography_na.png'), dpi=200, width=6, height=4)
 
 # -----------------------------------------
 # plot sequence qualities (only if already run through FAIL/PASS
@@ -71,7 +72,7 @@ if (all(FAIL_PASS_FIELDS %in% colnames(x))) {
   x[x$Status=='Too short', "Status"] <- "Less than 28kb"
   p.passfail <- ggplot(x, aes(x=Status, fill=Status)) + geom_bar() +
     xlab("") + ylab("Count") + labs(title="Sequence quality")
-  ggsave(file.path(report.figdir,'Rplot.pass_fail.png'), dpi=200, width=4.5, height=2.5)
+  #ggsave(file.path(report.figdir,'Rplot.pass_fail.png'), dpi=100, width=4.5, height=2.5)
 
   # number of 'N's
   x$NCountCat <- "0"
@@ -83,7 +84,7 @@ if (all(FAIL_PASS_FIELDS %in% colnames(x))) {
   x$NCountCat <- ordered(x$NCountCat, levels=c("0", "1-10", "11-100", "101-1000", ">1000"))
   p.Ns <- ggplot(x, aes(NCountCat, fill=NCountCat)) + geom_bar() + xlab("") + ylab("Count") +
     labs(title="Number of 'N's in sequences")
-  ggsave(file.path(report.figdir,'Rplot.count_Ns.png'), dpi=200, width=4.5, height=2.5)
+  #ggsave(file.path(report.figdir,'Rplot.count_Ns.png'), dpi=200, width=6, height=4)
 
   # number of segments
   x$Segments <- "1"
@@ -92,9 +93,9 @@ if (all(FAIL_PASS_FIELDS %in% colnames(x))) {
   x[x$GapCount>=5, "Segments"] <- "6+"
   x$Segments <- as.factor(x$Segments)
   x$Segments <- ordered(x$Segments, levels=c("1", "2", "3-5", "6+"))
-  p.segments <- ggplot(x, aes(Sequencing.technology, fill=Segments)) + geom_bar() + xlab("") + ylab("Count") +
+  p.segments <- ggplot(x, aes(Segments, fill=Segments)) + geom_bar() + xlab("") + ylab("Count") +
     labs(title="Number of continuous (non-N) sequence segments")
-  ggsave(file.path(report.figdir,'Rplot.count_segments.png'), dpi=200, width=4.5, height=2.5)
+  #ggsave(file.path(report.figdir,'Rplot.count_segments.png'), dpi=100, width=4.5, height=2.5)
 
 }
 

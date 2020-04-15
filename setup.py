3fe3b71f0b3e1c3631151c6b88dff187aad814bc
Magdoll <magdoll@gmail.com>
2020-04-15 14:43:58 -0700

    v2.0.0

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(
                'cosa/filter_gappedshort.py',
 			   'cosa/utils/fetch_NCBI.py',
                'cosa/pacbio/subsample_amplicons.py',
+               'cosa/pacbio/juliet_json_to_vcf.py',
                ],
     )

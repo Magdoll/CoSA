0d0db82653c8a0838fb77b59f41e6a9fa6991ee1
Magdoll <magdoll@gmail.com>
2021-02-17 22:49:47 -0800

    commit

--- vcf/run_pbaa.sh
+++ vcf/run_pbaa.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/bash
 
-SAMPLE=$1
+# ##################################################
+#
+# INPUT: a post-patient-demux BAM file (still has the amplicon primers)
+# OUTPUT: pbaa VCF file <SAMPLE>.vcf
+#
+# ##################################################
 
-REF=/home/UNIXHOME/etseng/projects2020/SARS_Cov2/LabCorp/Jan2021_VCFcons_forCDC/NC_045512.guide_for_pbaa.fasta
-REF2=/home/UNIXHOME/etseng/projects2020/SARS_Cov2/LabCorp/Jan2021_VCFcons_forCDC/NC_045512.fa
-PROG2TABLE=~/GitHub/CoSA/vcf/consensusVariants.py
-PROG2VCF=~/GitHub/CoSA/vcf/pbaa2vcf.py
+INPUT_BAM=$1   # ex: <full_path>/demultiplexing_files/demultiplex.M13_bc1027_F--M13_bc1075_R.bam
+SAMPLE=$2      # ex: LC00012
+PRIMERS=/data/sarscov2_primers.fasta 
+GENOME=/data/NC_045512.2.fasta
+PBAA_REF=/data/sarscov2_guide_for_pbaa.fasta
+PROG2TABLE=consensusVariants.py
+PROG2VCF=pbaa2vcf.py
 
-bamtools convert -format fastq -in ${SAMPLE}.bam > ${SAMPLE}.fastq
-samtools faidx ${SAMPLE}.fastq
-pbaa cluster --min-cluster-read-count 2 --trim-ends 0 --log-level DEBUG $REF --log-file ${SAMPLE}.log ${SAMPLE}.fastq $SAMPLE
-python $PROG2TABLE -r $SAMPLE -p $SAMPLE --read_info ${SAMPLE}_read_info.txt --hifiSupport ${SAMPLE}.fastq $REF2 ${SAMPLE}_passed_cluster_sequences.fasta
-python $PROG2VCF --passOnly -s barcode -o ${SAMPLE}.vcf ${SAMPLE}_alleles.csv ${SAMPLE}_variants.csv $REF2
+
+bamtools convert -format fastq -in sample.bam > sample.fastq
+samtools faidx sample.fastq
+
+# run pbaa
+pbaa cluster --min-cluster-read-count 2 --trim-ends 0 $PBAA_REF --log-file pbaa.log sample.fastq $SAMPLE
+# convert pbaa outcome to VCF
+$PROG2TABLE -r $SAMPLE -p $SAMPLE --read_info ${SAMPLE}_read_info.txt --hifiSupport sample.fastq $GENOME ${SAMPLE}_passed_cluster_sequences.fasta
+$PROG2VCF --passOnly -s barcode -o ${SAMPLE}.vcf ${SAMPLE}_alleles.csv ${SAMPLE}_variants.csv $GENOME
 rm -rf ${SAMPLE}*.dot
